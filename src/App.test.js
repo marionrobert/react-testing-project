4c3eae42d4efedb5efd4493dd4a38ef0d91bc554
Marion Robert <mrobert148@gmail.com>
2024-02-26 16:12:56 -0500

    test ok

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("codesweetly test heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading")).toHaveTextContent(/codesweetly test/i);
+  });
+
+  test("a codesweetly project heading", async () => {
+    render(<App />);
+
+    const button = screen.getByRole("button", { name: "Update Heading" });
+
+    userEvent.click(button);
+
+    // Utilisez waitFor pour attendre que le titre soit mis à jour
+    await waitFor(() => {
+      expect(screen.getByRole("heading")).toHaveTextContent(/a codesweetly project/i);
+    }, { timeout: 3000 });
+  });
 });
+
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import "@testing-library/jest-dom";
+// import App from "./App";
+
+// describe("App component", () => {
+//   test("codesweetly test heading", () => {
+//     render(<App />);
+//     expect(screen.getByRole("heading")).toHaveTextContent(/codesweetly test/i);
+//   });
+
+//   test("a codesweetly project heading", () => {
+//     render(<App />);
+
+//     const button = screen.getByRole("button", { name: "Update Heading" });
+
+//     userEvent.click(button);
+
+//     expect(screen.getByRole("heading")).toHaveTextContent(/a codesweetly project/i);
+//   });
+// });
