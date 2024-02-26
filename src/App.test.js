4395dd579bb8d087b1ff693777128c9c4e755681
Marion Robert <mrobert148@gmail.com>
2024-02-26 16:27:12 -0500

    testing with waitFor method

--- src/App.test.js
+++ src/App.test.js
@@ -20,29 +20,6 @@ describe("App component", () => {
     // Utilisez waitFor pour attendre que le titre soit mis à jour
     await waitFor(() => {
       expect(screen.getByRole("heading")).toHaveTextContent(/a codesweetly project/i);
-    }, { timeout: 3000 });
+    });
   });
 });
-
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import "@testing-library/jest-dom";
-// import App from "./App";
-
-// describe("App component", () => {
-//   test("codesweetly test heading", () => {
-//     render(<App />);
-//     expect(screen.getByRole("heading")).toHaveTextContent(/codesweetly test/i);
-//   });
-
-//   test("a codesweetly project heading", () => {
-//     render(<App />);
-
-//     const button = screen.getByRole("button", { name: "Update Heading" });
-
-//     userEvent.click(button);
-
-//     expect(screen.getByRole("heading")).toHaveTextContent(/a codesweetly project/i);
-//   });
-// });
